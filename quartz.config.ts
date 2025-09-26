41cf4acccf7b0c41556082a7aab43d3322465383
pink10000 <kyletrinh1223@gmail.com>
2025-09-26 15:22:23 -0700

    update submodule name

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "pink10000.github.io/obsidian-notes",
+    baseUrl: "pink10000.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {

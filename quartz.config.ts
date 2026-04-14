4f69e03fbbfec913d37bef19d8f48335126ab3de
pink10000 <kyletrinh1223@gmail.com>
2026-04-13 22:33:46 -0700

    update preamble

--- quartz.config.ts
+++ quartz.config.ts
@@ -130,6 +130,7 @@ const config: QuartzConfig = {
           "\\vvf": "\\textbf{f}",
           "\\rrarrow": "\\rightrightarrows",
           "\\ceil": "\\left\\lceil #1 \\right\\rceil",
+          "\\llbracket": "[\![", "\\rrbracket": "]\!]",
 
           // Physics
           "\\dimM": "\\mathsfup{M}",
